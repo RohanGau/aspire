a24fb7f936a8d76ccfef298f13bab5a3671c1650
amitjoc <amit.joshi@gorapid.io>
2025-04-26 01:51:40 +0530

    installed sass

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/teleparty-chat/',
+  base: '/aspire/',
   build: {
     outDir: 'dist'
   }
